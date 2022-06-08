310c7235548778a4bacf6549661167ca06c26e76
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-06-08 13:21:10 +0200

    info section added

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import backgroundimage from "../images/background-filter.webp";
 import vinylImg from "../images/vinyl.svg";
+import hwaitingSign from "../images/hwaiting2.svg";
 
 export default function Home(props) {
   return (
@@ -23,6 +24,38 @@ export default function Home(props) {
         <p>화이팅</p> <p>HWAITING</p> <p>화이팅</p> <p>HWAITING</p>{" "}
         <p>화이팅</p>
       </section>
+
+      <h2 className="pm_font">Meaning of Hwaiting</h2>
+      <section id="home-info">
+        <div className="home-info-container">
+          <div className="left-col">
+            <p className="dark-text">
+              Hwaiting (파이팅) is a korean word for support or encouragement.{" "}
+              <br /> <br />
+              This festival was created as a way to celebrate and support the
+              creativity, hard work and talent of korean artists and K-pop
+              music. <br />
+              <br />
+              Based in Denmark, Hwaiting brings this colorful and energetic
+              culture phenomenon to Northern Europe by hosting the biggest K-pop
+              festival in Scandinavia! <br /> <br />
+              Join the festival to meet up with other K-pop lovers from all
+              around the world to share your love and excitement for your
+              favorite artists with others.
+            </p>
+          </div>
+
+          <div className="home-info-graphics">
+            <img
+              className="home-info-img"
+              src={hwaitingSign}
+              alt="hwaiting written with korean signs"
+            ></img>
+            <p className="dark-text"> Pronounced [pʰaitʰiŋ]</p>
+          </div>
+        </div>
+      </section>
+
       <section id="home-program-section">
         <div>
           <h2 className="pm_font">Curious about the lineup?</h2>
